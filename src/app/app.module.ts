61660ab588dc0487af8af8236223bce3a300327b
akhilpy <akhilendra44@gmail.com>
2017-10-11 01:24:06 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', component: DashboardComponent },
+    { path: 'signup',  component: SignupComponent},
+    { path: 'login',   component: LoginComponent },
+    { path: 'footer',  component: FooterComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+ const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule,
+    routing
+
   ],
   providers: [],
   bootstrap: [AppComponent]
